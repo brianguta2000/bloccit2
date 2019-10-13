b9c750866e810024145be29d6563a6c1d82241f0
unknown <brianguta2000@yahoo.com>
2019-10-13 13:03:45 -0400

    database

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -9,20 +9,17 @@ describe("routes : static", () => {
   describe("GET /", () => {
 
 //#2
-<<<<<<< HEAD
+
     it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", (done) => {
-=======
+
   it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", (done) => {
->>>>>>> topic-resource
+
 
 //#3
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
         expect(body).toContain("Welcome to Bloccit");
-<<<<<<< HEAD
 
-=======
->>>>>>> topic-resource
 //#4
         done();
       });
--- src/routes/static.js
+++ src/routes/static.js
@@ -1,12 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const staticController = require("../controllers/staticController");
-<<<<<<< HEAD
 
 router.get("/", staticController.index);
-
-
-=======
-router.get("/", staticController.index);
->>>>>>> topic-resource
 module.exports = router;
