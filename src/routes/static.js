41e02b684f98b98ec4d4d2b7bee484a81b931753
unknown <brianguta2000@yahoo.com>
2019-10-13 13:26:24 -0400

    database

--- src/routes/static.js
+++ src/routes/static.js
@@ -1,12 +1,5 @@
 const express = require("express");
 const router = express.Router();
 const staticController = require("../controllers/staticController");
-<<<<<<< HEAD
-
 router.get("/", staticController.index);
-
-
-=======
-router.get("/", staticController.index);
->>>>>>> topic-resource
 module.exports = router;
