9761e98b62ec8fabdc4f452a66a96039426c8476
unknown <brianguta2000@yahoo.com>
2019-10-15 19:34:11 -0400

    topic2

--- src/controllers/topicController.js
+++ src/controllers/topicController.js
@@ -1,15 +1,75 @@
 const topicQueries = require("../db/queries.topics.js");
+
 module.exports = {
-  index(req, res, next){
+    index(req, res, next) {
+        topicQueries.getAllTopics((err, topics) => {
+            console.log(topics + "!!!!!!!!")
+            if (err) {
+                console.log("Internal Server Error: ")
+                console.log(err);
+                console.log("\n\n");
+                res.redirect(500, "static/index");
+            } else {
+                res.render("topics/index", { topics });
+            }
+        })
+    },
+    new(req, res, next) {
+        res.render("topics/new");
+    },
+    create(req, res, next){/* action implementation */},
 
-    topicQueries.getAllTopics((err, topics) => {
+     show(req, res, next){
 
-//#3
-        if(err){
-          res.redirect(500, "static/index");
-        } else {
-          res.render("topics/index", {topics});
-        }
-      })
-  }
+  //#1
+       topicQueries.getTopic(req.params.id, (err, topic) => {
+
+  //#2
+         if(err || topic == null){
+           res.redirect(404, "/");
+         } else {
+           res.render("topics/show", {topic});
+         }
+       });
+     },
+
+    show(req, res, next) {
+        topicQueries.getTopic(req.params.id, (err, topic) => {
+            if (err || topic == null) {
+                res.redirect(404, "/");
+            } else {
+                res.render("topics/show", { topic });
+            }
+        });
+    },
+  
+    destroy(req, res, next) {
+        topicQueries.deleteTopic(req.params.id, (err, topic) => {
+            if (err) {
+                res.redirect(500, `/topics/${topic.id}`)
+            } else {
+                res.redirect(303, "/topics")
+            }
+        });
+    },
+    edit(req, res, next) {
+        topicQueries.getTopic(req.params.id, (err, topic) => {
+            if (err || topic == null) {
+                res.redirect(404, "/");
+            } else {
+                res.render("topics/edit", { topic });
+            }
+        });
+    },
+    update(req, res, next) {
+
+        topicQueries.updateTopic(req.params.id, req.body, (err, topic) => {
+
+            if (err || topic == null) {
+                res.redirect(404, `/topics/${req.params.id}/edit`);
+            } else {
+                res.redirect(`/topics/${topic.id}`);
+            }
+        });
+    }
 }
--- src/routes/static.js
+++ src/routes/static.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const staticController = require("../controllers/staticController");
-<<<<<<< HEAD
-=======
 
->>>>>>> database
+
+
+
 router.get("/", staticController.index);
 module.exports = router;
